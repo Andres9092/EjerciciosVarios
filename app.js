02e6729186ac3302229f77ac270a45f0ef29cff7
Andres9092 <richardsonandres1@gmail.com>
2022-10-08 19:47:35 -0300

    ejercicio Nav Bar

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+
+
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')))  //se puede prescidir de este termino __dirname//
+
+
+
+app.listen(3000, ()=>{
+    console.log('Servidor levantado');
+});
+
+
+app.get('/', (req,res)=>{
+   res.sendFile(__dirname + '/views/index.html');
+})
+
+app.get('/productDetail', (req,res)=>{
+    res.sendFile(__dirname + '/views/productDetail.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/carrito', (req,res)=>{
+    res.sendFile(__dirname + '/views/carrito.html');
+})
+
+app.get('/test', (req,res)=>{
+    res.sendFile(__dirname + '/views/test.html');
+})
+
+app.get('/menu', (req,res)=>{
+    res.sendFile(__dirname + '/views/menu.html');
+})
